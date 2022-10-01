1def2d78b4261d5d69a13c4c93573bba4d5e6144
Camila Maraschin <105385268+camilamaraschin@users.noreply.github.com>
2022-10-01 17:33:42 -0300

    Add files via upload

---
+++ Desafio 01/main.js
@@ -0,0 +1,25 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroQuarenta = 40
+let stringTrinta = '30'
+let numeroDez = 10
+let stringNoventa = '90'
+
+
+if(numeroUm !== stringUm){
+    console.log('As variáveis numero 1 e string 1 tem o mesmo valor, mas tipos diferentes')
+} else{
+    console.log('As variáveis numero 1 e string 1 não tem o mesmo valor')
+}
+if (numeroQuarenta == stringTrinta) {
+    console.log('As variáveis numero 40 e string 30  tem o mesmo valor e mesmo tipo')
+  } else {
+    console.log('As variáveis numero 40 e string 30 não tem o mesmo tipo')
+  }
+  
+  if (numeroDez === stringNoventa ) {
+    console.log('As variáveis numero 10 e string 90 tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numero 10 e string 90 não tem o mesmo valor')
+  }
+  
